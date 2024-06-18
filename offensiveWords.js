548acc31cea3cf89e2cc3ecdbdbd9e30c53629d6
Mohd Mahmodi <141009456+MohdYahyaMahmodi@users.noreply.github.com>
2024-06-18 05:21:45 -0700

    Update offensiveWords.js

--- offensiveWords.js
+++ offensiveWords.js
@@ -1,12 +1,12 @@
 // offensiveWords.js
 const OFFENSIVE_WORDS = [
-  'nword', // Example placeholder, replace with actual offensive word
-  'offensiveWord1', // Replace with actual offensive word
-  'offensiveWord2', // Replace with actual offensive word
-  'offensive phrase', // Replace with actual offensive phrase
-  'another offensive phrase', // Replace with actual offensive phrase
-  'racial slur', // Replace with actual racial slur
-  'curse word', // Replace with actual curse word
+  'nigga', // Example placeholder, replace with actual offensive word
+  'nigger', // Replace with actual offensive word
+  'n i g g a ', // Replace with actual offensive word
+  'faggot', // Replace with actual offensive phrase
+  'faggot', // Replace with actual offensive phrase
+  'kill yourslef', // Replace with actual racial slur
+  'kill urslef', // Replace with actual curse word
   'derogatory term', // Replace with actual derogatory term
   'insulting phrase', // Replace with actual insulting phrase
   // Add more words and phrases as needed
