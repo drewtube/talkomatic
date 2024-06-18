ff61ba094ef825f2bda9e4a5824abe70e24c3de4
Mohd Mahmodi <141009456+MohdYahyaMahmodi@users.noreply.github.com>
2024-06-18 06:30:28 -0700

    final

--- server.js
+++ server.js
@@ -28,8 +28,6 @@ app.get('/banned', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('A user connected:', socket.id);
-
     socket.on('userConnected', (data) => {
         const { userId } = data;
         if (isUserBanned(userId)) {
@@ -52,19 +50,14 @@ io.on('connection', (socket) => {
     socket.on('createRoom', (roomData) => {
         const { username, location, userId, name, type, layout } = roomData;
 
-        // Debugging statements
-        console.log('Received room creation request:', roomData);
-
         // Validate inputs
         if (!username || !location || !userId || !name || !['public', 'private', 'secret'].includes(type) || !['horizontal', 'vertical'].includes(layout)) {
             socket.emit('error', 'Invalid input');
-            console.log('Room creation failed - invalid input:', socket.id, roomData);
             return;
         }
 
         if (username.length > MAX_CHAR_LENGTH || location.length > MAX_CHAR_LENGTH || name.length > MAX_CHAR_LENGTH) {
             socket.emit('error', 'Input exceeds maximum length');
-            console.log('Room creation failed - input exceeds maximum length:', socket.id, roomData);
             return;
         }
 
@@ -87,7 +80,6 @@ io.on('connection', (socket) => {
         socket.join(roomId);
         socket.emit('roomJoined', { roomId, username: sanitizedUsername, location: sanitizedLocation, userId: sanitizedUserId });
         socket.emit('initializeUsers', room.users);
-        console.log('Room created:', roomId, 'by user:', socket.id);
 
         updateCounts();
     });
@@ -95,19 +87,14 @@ io.on('connection', (socket) => {
     socket.on('joinRoom', (data) => {
         const { roomId, username, location, userId } = data;
 
-        // Debugging statements
-        console.log('Received join room request:', data);
-
         // Validate inputs
         if (!roomId || !username || !location || !userId) {
             socket.emit('error', 'Invalid input');
-            console.log('Room join failed - invalid input:', socket.id, data);
             return;
         }
 
         if (username.length > MAX_CHAR_LENGTH || location.length > MAX_CHAR_LENGTH || roomId.length > MAX_CHAR_LENGTH) {
             socket.emit('error', 'Input exceeds maximum length');
-            console.log('Room join failed - input exceeds maximum length:', socket.id, data);
             return;
         }
 
@@ -116,7 +103,6 @@ io.on('connection', (socket) => {
         const sanitizedLocation = escapeHtml(location);
         const sanitizedUserId = escapeHtml(userId);
 
-        console.log('User', socket.id, 'is joining room:', roomId);
         if (rooms[roomId]) {
             // Clear any existing deletion timeout for the room
             if (roomDeletionTimeouts[roomId]) {
@@ -131,15 +117,13 @@ io.on('connection', (socket) => {
                 socket.emit('roomJoined', { roomId, username: sanitizedUsername, location: sanitizedLocation, userId: sanitizedUserId });
                 socket.emit('initializeUsers', rooms[roomId].users);
                 socket.to(roomId).emit('userJoined', { roomId, username: sanitizedUsername, location: sanitizedLocation, userId: sanitizedUserId });
-                console.log('User', socket.id, 'joined room:', roomId);
 
                 updateCounts();
             } else {
                 socket.emit('roomFull');
-                console.log('Room', roomId, 'is full. User', socket.id, 'cannot join.');
             }
         } else {
-            console.log('Room', roomId, 'does not exist. User', socket.id, 'cannot join.');
+            socket.emit('roomNotFound');
         }
     });
 
@@ -161,11 +145,9 @@ io.on('connection', (socket) => {
         }
 
         socket.to(roomId).emit('typing', { userId, message: sanitizedMessage });
-        console.log('User', userId, 'is typing in room:', roomId);
     });
 
     socket.on('disconnect', () => {
-        console.log('A user disconnected:', socket.id);
         Object.keys(rooms).forEach((roomId) => {
             const room = rooms[roomId];
             const userIndex = room.users.findIndex((user) => user.socketId === socket.id);
@@ -173,14 +155,12 @@ io.on('connection', (socket) => {
                 const user = room.users.splice(userIndex, 1)[0];
                 io.emit('roomUpdated', room);
                 socket.to(roomId).emit('userLeft', { roomId, userId: user.userId });
-                console.log('User', user.userId, 'left room:', roomId);
 
                 // Start the deletion timeout if there are no users left in the room
                 if (room.users.length === 0) {
                     roomDeletionTimeouts[roomId] = setTimeout(() => {
                         delete rooms[roomId];
                         io.emit('roomRemoved', roomId);
-                        console.log('Room removed due to inactivity:', roomId);
                         delete roomDeletionTimeouts[roomId];
                     }, 10000); // 10 seconds delay
                 }
@@ -226,6 +206,4 @@ function getBanExpiration(userId) {
 }
 
 const port = process.env.PORT || 3000;
-server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+server.listen(port);
