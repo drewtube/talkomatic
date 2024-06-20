90240ef7945dde65858c06f1e79481d328f6ec03
Mohd Mahmodi <141009456+MohdYahyaMahmodi@users.noreply.github.com>
2024-06-20 15:05:07 -0700

    word list update

--- server.js
+++ server.js
@@ -277,7 +277,7 @@ function updateCounts() {
 }
 
 function generateRoomId() {
-    return '_' + Math.random().toString(36).substr(2, 9);
+    return Math.floor(100000 + Math.random() * 900000).toString();
 }
 
 function containsOffensiveWords(message) {
