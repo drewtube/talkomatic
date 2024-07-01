447f6fdb1a9aa14246c488150d8be79287e2ea6b
Mohd Mahmodi <141009456+MohdYahyaMahmodi@users.noreply.github.com>
2024-07-01 10:20:55 -0700

    new mod

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const birthdayCelebrated = new Map();
 const MAX_CHAR_LENGTH = 20;
 
 // Define allowed moderators
-const allowedMods = ['user_j369j5gkz','user_crzuhudgz','user_cc7r68nu0','user_pl43x2vca','user_1csrwjjd7','user_030skg48h','user_f379qlasz','user_nict8jm8e',' Rea1%D0%9C%D0%BEd%D0%B5pOx%D1%83%D0%B5t69p%D0%B0shal%D0%BA%D0%BE']; // Add more user IDs as needed
+const allowedMods = ['user_j369j5gkz','user_crzuhudgz','user_cc7r68nu0','user_pl43x2vca','user_1csrwjjd7','user_030skg48h','user_f379qlasz','user_nict8jm8e','user_7zl2kndsl']; // Add more user IDs as needed
 
 app.use(express.static(path.join(__dirname, '../')));
 app.use(cookieParser());
